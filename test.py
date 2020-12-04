a784a67f7565afb37880f2986eab356ed51b8157
AlexisD29 <55716742+AlexisD29@users.noreply.github.com>
2020-12-04 13:51:25 -0500

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import ftplib
-host = 'localhost'
-port = 8080
-ftp = ftplib.FTP()
-ftp.connect(host,port)
-ftp.login('SE','123')
-uploadfile= open('C:\\Users\\alexi\\Server\\upload.html', 'upload.html')
-
-ftp.storlines('STOR ' + filename, uploadfile)
